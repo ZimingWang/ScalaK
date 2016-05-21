0454dd38eca08ed6f236e4dc8f07ff5e811b0b2f
zimingwang <ziming.wang@west.cmu.edu>
2016-05-21 12:51:30 -0700

    fp start

--- src/Entry/TheTraits.scala
+++ src/Entry/TheTraits.scala
@@ -2,9 +2,9 @@
   * Created by wangziming on 5/21/16.
   */
 
-trait ForEachAble[A] {
+trait ForEachAble[A] {//generics
   def iterator: java.util.Iterator[A]
-  def foreach(f: A => Unit) = {
+  def foreach(f: A => Unit) = {//functions
     val iter = iterator
     while (iter.hasNext)
       f(iter.next)
@@ -12,12 +12,23 @@ trait ForEachAble[A] {
 }
 
 trait JsonAble {
-  def toJson() =
-    scala.util.parsing.json.JSONFormat.defaultFormatter(this)
+  def toJson() = {
+
+  }
+    //scala.util.parsing.json.JSONFormat.defaultFormatter(this)//being removed
+
 }
 
-val list = new java.util.ArrayList[Int]() with ForEachAble[Int]
-list.add(1); list.add(2)
 
-println("For each: "); list.foreach(x => println(x))
-//println("Json: " + list.toJson())
+object TheTraits {
+
+  def main(argv : Array[String]): Unit ={
+    val list = new java.util.ArrayList[Int]() with ForEachAble[Int] with JsonAble
+    list.add(1); list.add(2)
+
+    println("For each: "); list.foreach(x => println(x))
+    println("Json: " + list.toJson())
+
+  }
+
+}
---
+++ src/FP/PatternM.scala
@@ -0,0 +1,29 @@
+/**
+  * Created by wangziming on 5/21/16.
+  */
+
+class PatternMM{
+
+  def fibonacci(in: Any) : Int = in match {
+    case 0 => 0patternMM.
+    case 1 => 1
+    case n: Int if(n > 1) => fibonacci(n - 1) + fibonacci(n - 2)
+    case n: String => fibonacci(n.toInt)
+    case _ => 0
+  }
+}
+
+
+object PatternM{
+  def main(argv : Array[String]): Unit ={
+
+    val patternMM = new PatternMM()
+    println(patternMM.fibonacci(3))
+    println(patternMM.fibonacci(-3))
+    println(patternMM.fibonacci("3"))
+  }
+}
+
+
+
+
