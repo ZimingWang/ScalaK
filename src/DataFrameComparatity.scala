9b66b24d95a69d1c4d88a102f43cdb10734b11ea
zimingwang <ziming.wang@west.cmu.edu>
2016-06-02 16:22:37 -0700

    spark app

--- src/DataFrameComparatity.scala
+++ src/DataFrameComparatity.scala
@@ -2,6 +2,8 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
+
+
 /**
   * Created by wangziming on 5/31/16.
   */
@@ -21,16 +23,49 @@ import org.apache.spark.SparkConf
 
 
 //Basic Code
-object DataFrameComparatity {
+object DataFrameComparatity{
   def main(args: Array[String]) {
 
-    val logFile = "/Users/wangziming/Desktop/spark-1.5.2/README.md" // Should be some file on your system
+    /*val logFile = "/Users/wangziming/Desktop/spark-1.5.2/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))*/
+
+    val conf = new SparkConf().setAppName("DataFrame Equality Check")
+    val sc = new SparkContext(conf)
+    //Read two dataframes from json,
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val df1 = sqlContext.read.json("/Users/wangziming/Desktop/spark-1.6.0-bin-hadoop2.6/examples/src/main/resources/people.json")
+    val df2 = sqlContext.read.json("/Users/wangziming/Desktop/spark-1.6.0-bin-hadoop2.6/examples/src/main/resources/people.json")
+    //Read column names from dataframes
+    val df1_column = df1.columns
+    val df2_column = df2.columns
+
+    scala.util.Sorting.quickSort(df1_column)
+    scala.util.Sorting.quickSort(df2_column)
+
+    val length1 = df1_column.length
+    val length2 = df2_column.length
+
+    if(length1 != length2){
+      println(false + "\tlength of attributes are not equal")
+    }else{
+      println(true + "\tlength of attributes are equal")
+    }
+
+    for(i <- 0 to length1 - 1){
+      if(df1_column(i) != df1_column(i)){
+        println(false)
+      }else{
+        println(true)
+      }
+    }
+    //all thetypes
 
   }
 }
